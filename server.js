dbaacaf4fbc04c62a995fef35386cf51a49f0e6b
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-05-21 16:34:55 +0200

    Esercizio base finito

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
+
+
+const { config } = require('dotenv');
 const http = require("http");
-const host = process.env.host || "localhost";
-const port = process.env.port || 5000;
+const host = process.env.host || "localhost2";
+const port = process.env.port || 6000;
 
 http
     .createServer(function (req, res) {
         res.writeHead(200, {
             "content-Type": "text/html"
         });
-        res.end("<h1 style='color: red;'>Hello World</h1>");
+        res.end(
+            `<h1 style="color: red;">L'host: ${host}</h1>
+            <h1 style="color: red;">La porta: ${port}</h1>`    
+    );  
     })
     .listen(port, host, () => {
-        const serverUrl = 'http://${host}:${port}'
+        const serverUrl = `http://${host}:${port}`
     })
